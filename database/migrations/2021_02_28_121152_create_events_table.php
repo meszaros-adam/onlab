956f3cc71678dac7ec977e68f36c4d5dc17c396b
Mészáros Ádám <meszike91@gmail.com>
2021-02-28 13:39:49 +0100

    event model, controller, migration

---
+++ database/migrations/2021_02_28_121152_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->date('date');
+            $table->string('name');
+            $table->text('description');
+            $table->integer('number_of_people');
+            $table->string('location');
+            
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
