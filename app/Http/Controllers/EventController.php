867c019322df986eef33c43f0595cd23bc430108
Mészáros Ádám <meszike91@gmail.com>
2021-02-28 16:53:34 +0100

    eventek listazasa

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Models\Event;
+
 
 class EventController extends Controller
 {
-    //
+    public function listing(){
+        $events=Event::all();
+        return $events;
+    }
 }
