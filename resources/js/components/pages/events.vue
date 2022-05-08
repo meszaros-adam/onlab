d3b0ce7e90ff03a29622c4c5bf71670b1ce46e64
Mészáros Ádám <meszike91@gmail.com>
2022-05-08 20:24:40 +0200

    regModal events.vue

--- resources/js/components/pages/events.vue
+++ resources/js/components/pages/events.vue
@@ -5,16 +5,22 @@
         <div class="card text-dark">
           <div class="card-body">
             <h5 class="card-title">{{ event.name }}</h5>
-            <p class="card-text">Leírás: {{ event.description }}</p>
+            <p class="card-text">{{ event.description }}</p>
           </div>
           <ul class="list-group list-group-flush">
             <li class="list-group-item">Időpont: {{ event.date }}</li>
             <li class="list-group-item">Helyszín: {{ event.location }}</li>
-            <li class="list-group-item">Létszám: {{ event.headcount }}</li>
-            <li class="list-group-item">Szabad helyek: {{ event.free_seats }}</li>
+            <li class="list-group-item">
+              <strong>Létszám: {{ event.headcount }}</strong>
+            </li>
+            <li class="list-group-item">
+              <strong>Szabad helyek: {{ event.free_seats }}</strong>
+            </li>
           </ul>
           <div class="card-body text-end">
-            <div class="button btn btn-success">Regisztrálás</div>
+            <button class="button btn btn-success" @click="showRegModal(event)">
+              Regisztrálás
+            </button>
           </div>
         </div>
       </div>
@@ -28,19 +34,77 @@
       @change="handlePageChange"
     ></b-pagination>
     <!-- Pagination -->
+
+    <!-- Registration model-->
+    <b-modal
+      v-model="regModal"
+      title="Regisztráció: "
+      hide-header-close
+      hide-footer
+      no-close-on-esc
+      no-close-on-backdrop
+    >
+      <div class="mb-3">
+        <label class="form-label">Hány fővel szeretne regisztrálni? </label>
+        <input
+          v-if="regModal"
+          class="form-control"
+          type="number"
+          min="1"
+          :max="regEvent.free_seats"
+          v-model="data.headcount"
+        />
+      </div>
+
+      <div class="d-flex justify-content-end">
+        <button
+          type="button"
+          class="btn btn-danger mx-2"
+          @click="closeRegModal()"
+        >
+          Bezárás
+        </button>
+        <button
+          type="button"
+          :disabled="registrating"
+          @click="register()"
+          class="btn btn-success mx-2"
+        >
+          <span
+            class="spinner-border spinner-border-sm"
+            role="status"
+            aria-hidden="true"
+            v-show="registrating"
+          ></span>
+          <span class="sr-only">Regisztráció</span>
+        </button>
+      </div>
+    </b-modal>
+    <!-- Registration model-->
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   data() {
     return {
+      data: {
+        headcount: "",
+        userId: null,
+        eventId: null,
+      },
       events: [],
 
       //pagination
       itemPerPage: 10,
       currentPage: 1,
       total: 0,
+
+      regModal: false,
+      registrating: false,
+      regEvent: null,
     };
   },
   methods: {
@@ -56,15 +120,44 @@ export default {
       } else {
         this.$toast.error("Események betöltése sikertelen!");
       }
-      window.scrollTo(0,0);
+      window.scrollTo(0, 0);
     },
     handlePageChange(value) {
       this.currentPage = value;
       this.getEvents();
     },
+    showRegModal(event) {
+      this.data.userId = this.getUser.id;
+      this.regEvent = event;
+      this.regModal = true;
+    },
+    closeRegModal() {
+      this.registrating = false;
+      this.data.headcount = "";
+      this.regModal = false;
+    },
+    async register() {
+      if (this.data.headcount.length == 0)
+        return this.$toast.warning("Létszám megadása kötelező!");
+
+      this.registrating = true;
+
+      const res = await this.callApi("post", "create_registration", this.data);
+
+      if (res.status == 201) {
+        this.$toast.success("Sikeres registráció!");
+      } else {
+        this.$toast.error("A regisztráció nem sikerült!");
+      }
+
+      this.registrating = false;
+    },
   },
   created() {
     this.getEvents();
+  },
+  computed: {
+    ...mapGetters(["getUser"]),
   },
 };
 </script>
